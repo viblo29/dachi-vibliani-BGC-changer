e0c0edf13b87afdbdea18667a0bd34b9c86f6d20
viblo29 <dachivibliani29@gmail.com>
2024-11-17 01:17:02 +0400

    2nd.

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 const buttonBGC = document.getElementById(`BGC`);
-const color = document.getElementById(`color`);
+const colorDiv = document.getElementById(`color`);
+const txt = document.getElementById(`txt`);
+const colorCode = document.getElementById(`colorCode`);
 
 buttonBGC.addEventListener(`click`, function(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = `#${randomColor}`;
 
-    color.innerHTML = `Your Random Color Code is: #${randomColor}`;
+    colorDiv.style.display = `flex`;
+
     buttonBGC.style.color = `#${randomColor}`;
-    color.style.color = `#${randomColor}`;
-})
+    color.style.color = `black`;
+
+    colorCode.innerHTML = `#${randomColor}`
+    colorCode.style.color = `#49a6ea`
+
+    colorCode.addEventListener(`mouseenter`, function(){
+        colorCode.style.color = `#${randomColor}`;
+    });
+    
+    
+});
 
 
+colorCode.addEventListener(`mouseleave`, function(){
+    colorCode.style.color = `#49a6ea`;
+});
+
+
+colorCode.addEventListener('click', function() {
+    const textToCopy = colorCode.textContent;
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('Color code copied to clipboard!');
+    }).catch((err) => {
+        console.error('Error copying text: ', err);
+    });
+});
